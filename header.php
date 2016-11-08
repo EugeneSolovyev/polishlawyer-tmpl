e28bf3d6f77ec50981a8f8d4ccebd582d495a333
EugeneSolovyev <eugenesolovyev92@gmail.com>
2016-11-08 15:23:51 +0300

    collapse-menu on mobile version had done

--- header.php
+++ header.php
@@ -3,6 +3,9 @@
 <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta charset="utf-8">
+    <meta name="design" content="sisyrinchium02@gmail.com">
+    <meta name="frontend" content="eugenesolovyev92@gmail.com">
+    <meta name="backend" content="">
     <title>
         <?php bloginfo('name'); ?>
     </title>
@@ -17,5 +20,11 @@
    </head>
    <body>
         <div class="navbar navbar-fixed-top">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar" aria-controls="exCollapsingNavbar" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars" aria-hidden="true"></i></button>
+            <div class="collapse" id="exCollapsingNavbar">
+                <div class="bg-inverse text-muted p-1">
+                    <?php wp_nav_menu( array('menu' => 'Main', 'container' => 'nav' )); ?>
+                </div>
+            </div>
             <?php wp_nav_menu( array('menu' => 'Main', 'container' => 'nav' )); ?>
         </div>
